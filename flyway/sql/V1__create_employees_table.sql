1792dc0269d279a199db748b6dfb92a4e376641e
Nishanka Donikeni <nishankadonikeni@macbookair.lan>
2026-02-21 14:15:08 -0500

    add workflow and catalog

---
+++ flyway/sql/V1__create_employees_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE EMPLOYEES (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(50),
+    department VARCHAR(50),
+    salary INT,
+    hire_date DATE
+);
---
+++ flyway/sql/V2__insertdata_to_employees_table.sql
@@ -0,0 +1,2 @@
+INSERT into Employees VALUES
+    ( '1001', 'Nishank', 'IT', '50000', '08-10-1987' );
